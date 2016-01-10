f1f9b8bc85e4919cde0f64399d00bd97130460b7
Karl Leswing <lilleswing@gmail.com>
2016-01-09 20:33:19 -0500

    Serve template from configuration folder

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "Creating ticket at /usr/local/bin/ticket"
 sudo cp ticket.sh /usr/local/bin/ticket
+mkdir -p ~/.ticket
+rm -rf ~/.ticket/*
+cp ticket_template ~/.ticket
 echo "To create a ticket run 'ticket' from a git root"
