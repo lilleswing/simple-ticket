a9e9f47c62c39a948227a86411ff2246ef556d8c
Karl Leswing <lilleswing@gmail.com>
2016-01-09 20:03:51 -0500

    New Ticket for search api

--- ticket.sh
+++ ticket.sh
@@ -2,5 +2,5 @@
 mkdir -p .ticket
 number=$((`date +%s`+`date +%-N`/1000000))
 cp .ticket/ticket_template .ticket/$number
-echo "Created new ticket $number, please fill in details"
+echo "Created new ticket .ticket/$number, please fill in details"
 
